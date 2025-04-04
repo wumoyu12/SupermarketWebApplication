6dfcf382f91263dfa7864ba053f8f2ab05dad165
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-04-04 14:10:07 -0400

    Create logoin.py

---
+++ logoin.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template, request, redirect
+import os.path
+from os import path
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("login.html")
+
+@app.route("/info",methods=["POST"])
+
+def GetInfo():
+    username = request.form.get("txtusername")
+    userpasswd = request.form.get("txtpassword")
+
+    if(username == "" or userpasswd == ""):
+        return render_template("login.html")
+    else:
+        ExistsFile()
+
+    if (exists == "n"):
+        msg = "Account not found! Create Account?"
+        
+def ExistsFile():
+    global exists
+    userfile = username + ".txt"
+    fileDir = os.path.dirname(os.path.realpath("__file__"))
+    fileexist = bool(path.exists(userfile))
+    
+    if (fileexist == False):
+        exists = "n"
+    else:
+        exists = "y"
+
+    
+if __name__ == "__main__":
+    app.run();
