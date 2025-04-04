e7cf7fbcbd8d8b1967645a07fe698f934bac25eb
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-04-04 14:08:42 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", InitControls);
+window.addEventListener("load", addListener);
+
+function InitControls()
+{
+	
+}
+
+function addListener()
+{
+	document.getElementById("btnsubmit").addEventListener("click",Submit)
+}
