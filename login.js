4446f7834988c604bc7e373083f259ee6277083b
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-04-10 09:59:40 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,1 @@
-window.addEventListener("load", InitControls);
-window.addEventListener("load", addListener);
 
-function InitControls()
-{
-	document.getElementById("txtusername").focus();
-}
-
-function addListener()
-{
-	document.getElementById("btnsubmit").addEventListener("click",Submit)
-}
