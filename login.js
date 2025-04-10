d13034429cf1d9314b14803ec0b79f44ef723a9c
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-04-10 10:00:02 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,30 @@
+window.addEventListener("load", InitControls);
+window.addEventListener("load", addListeners);
 
+function InitControls() {
+    document.getElementById("txtloginusername").value = "";
+    document.getElementById("txtloginpassword").value = "";
+    document.getElementById("txtsignupusername").value = "";
+    document.getElementById("txtsignuppassword").value = "";
+    document.getElementById("txtsignuprpassword").value = "";
+    document.getElementById("txtloginusername").focus();
+}
+
+function addListeners() {
+    document.getElementById("showsignup").addEventListener("click", showSignUp);
+    document.getElementById("showlogin").addEventListener("click", showLogIn);
+}
+
+function showSignUp(event) {
+    event.preventDefault();
+    document.getElementById("divlogin").style.display = "none";
+    document.getElementById("divsignup").style.display = "block";
+    document.getElementById("txtsignupusername").focus();
+}
+
+function showLogIn(event) {
+    event.preventDefault();
+    document.getElementById("divsignup").style.display = "none";
+    document.getElementById("divlogin").style.display = "block";
+    document.getElementById("txtloginusername").focus();
+}
