539fa3e958d97aaefc01e854c62488dd5c40570c
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-04-10 10:24:36 -0400

    Update logoin.py

--- logoin.py
+++ logoin.py
@@ -14,25 +14,6 @@ def GetInfo():
     username = request.form.get("txtusername")
     userpasswd = request.form.get("txtpassword")
 
-    if(username == "" or userpasswd == ""):
-        return render_template("login.html")
-    else:
-        ExistsFile()
-
-    if (exists == "n"):
-        msg = "Account not found! Create Account?"
-        
-def ExistsFile():
-    global exists
-    userfile = username + ".txt"
-    fileDir = os.path.dirname(os.path.realpath("__file__"))
-    fileexist = bool(path.exists(userfile))
-    
-    if (fileexist == False):
-        exists = "n"
-    else:
-        exists = "y"
-
     
 if __name__ == "__main__":
     app.run();
